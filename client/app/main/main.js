aee8f13df7f4f021d7750b7b6badfc92746073d5
desak29 <desak29@icloud.com>
2016-04-25 16:21:11 -0400

    pollNOW
    MEAN____YEOMAN____PollingApp.

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+angular.module('pollAppApp')
+    .config(function ($stateProvider) {
+    $stateProvider
+        .state('main', {
+        url: '/',
+        template: '<main></main>'
+    });
+});
+//# sourceMappingURL=main.js.map
