81545f4f9d08fa94f21061361dfd60e763a0d3f0
desak29 <desak29@icloud.com>
2016-04-25 15:00:11 -0400

    PollingApp
    angular routing

---
+++ client/app/admin/admin.module.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('pollAppApp.admin', [
+  'pollAppApp.auth',
+  'ui.router'
+]);
---
+++ client/test_typings/tsd.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="selenium-webdriver/selenium-webdriver.d.ts" />
+/// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="angular-protractor/angular-protractor.d.ts" />
